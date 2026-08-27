ece2737a103c3d86382c86d116e6748a56e0395c
NoobMaester <joshstilez@gmail.com>
2026-08-27 14:21:04 +0200

    register user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,17 +2,19 @@ import {Request, Response} from 'express';
 import { registerUser } from './auth.service';
 
 export async function register(req: Request, res: Response) {
+
+    
     try {
-        const user = await registerUser(req.body);
+         const user = await registerUser(req.body);
         
-        res.status(201).json({
-            message: "User registered successfully",
-            user,
-        });
-    } catch (error) {
-        console.error(error)
-        res.status(400).json({ 
-            message: error instanceof Error ? error.message : "Registration failed"
-        });
-    }
-}
+         res.status(201).json({
+             message: "User registered successfully",
+             user,
+         });
+     } catch (error) {
+         console.error(error)
+         res.status(400).json({ 
+             message: error instanceof Error ? error.message : "Registration failed"
+         });
+     }
+} 
